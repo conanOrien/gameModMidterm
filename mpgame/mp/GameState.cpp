5e7428e9530e07b3a8525432a917b6036d98fb40
conanOrien <ow5@njit.edu>
2016-03-17 02:46:55 -0400

    Changed GameState to end when Marine kills all hiders or when clock is run out

--- mpgame/mp/GameState.cpp
+++ mpgame/mp/GameState.cpp
@@ -896,78 +896,10 @@ void rvTeamDMGameState::Run( void ) {
 
 	switch( currentState ) {
 		case GAMEON: {
-			int team = ( ( gameLocal.mpGame.GetScoreForTeam( TEAM_MARINE ) >= gameLocal.serverInfo.GetInt( "si_fragLimit" ) ) ? TEAM_MARINE : ( ( gameLocal.mpGame.GetScoreForTeam( TEAM_STROGG ) >= gameLocal.serverInfo.GetInt( "si_fragLimit" ) ) ? TEAM_STROGG : -1 ) );
-			if( gameLocal.serverInfo.GetInt( "si_fragLimit" ) <= 0 ) {
-				// no fraglimit
-				team = -1;
+			if (gameLocal.mpGame.GetScoreForTeam( TEAM_MARINE ) == gameLocal.numClients - 1 || gameLocal.time - gameLocal.serverInfo.GetInt( "si_timeLimit" ) * 60000 <= 0)
+			{
+				NewState( GAMEREVIEW );
 			}
-			bool tiedForFirst = gameLocal.mpGame.GetScoreForTeam( TEAM_MARINE ) == gameLocal.mpGame.GetScoreForTeam( TEAM_STROGG );
-			if ( team >= 0 && !tiedForFirst ) {
-				if ( !fragLimitTimeout ) {
-					common->DPrintf( "enter FragLimit timeout, team %d is leader\n", team );
-					fragLimitTimeout = gameLocal.time + FRAGLIMIT_DELAY;
-				}
-				if ( gameLocal.time > fragLimitTimeout ) {
-					NewState( GAMEREVIEW );
-
-					gameLocal.mpGame.PrintMessageEvent( -1, MSG_FRAGLIMIT, team );
-
-				}
-			} else if ( fragLimitTimeout ) {
-				// frag limit was hit and cancelled. means the two teams got even during FRAGLIMIT_DELAY
-				// enter sudden death, the next frag leader will win
-				//
-				// jshepard: OR it means that the winner killed himself during the fraglimit delay, and the
-				// game needs to roll on.
-				if( tiedForFirst )	{
-					//this is a tie
-					if( gameLocal.mpGame.GetScoreForTeam( TEAM_MARINE ) >= gameLocal.serverInfo.GetInt( "si_fragLimit" ) )	{
-						//and it's tied at the fraglimit.
-						NewState( SUDDENDEATH ); 
-					}
-					//not a tie, game on.
-					fragLimitTimeout = 0;
-				}
-			} else if ( gameLocal.mpGame.TimeLimitHit() ) {
-				gameLocal.mpGame.PrintMessageEvent( -1, MSG_TIMELIMIT );
-				if( tiedForFirst ) {
-					// if tied at timelimit hit, goto sudden death
-					fragLimitTimeout = 0;
-					NewState( SUDDENDEATH );
-				} else {
-					// or just end the game
-					NewState( GAMEREVIEW );
-				}
-			} else if( tiedForFirst && team >= 0 ) {
-				// check for the rare case that two teams both hit the fraglimit the same frame
-				// two people tied at fraglimit, advance to sudden death after a delay
-				fragLimitTimeout = gameLocal.time + FRAGLIMIT_DELAY;
-			}
-			break;
-		}
-
-		case SUDDENDEATH: {
-			int team = gameLocal.mpGame.GetScoreForTeam( TEAM_MARINE ) > gameLocal.mpGame.GetScoreForTeam( TEAM_STROGG ) ? TEAM_MARINE : TEAM_STROGG;
-			bool tiedForFirst = false;
-			if( gameLocal.mpGame.GetScoreForTeam( TEAM_MARINE ) == gameLocal.mpGame.GetScoreForTeam( TEAM_STROGG ) ) {
-				team = -1;
-				tiedForFirst = true;
-			}
-
-			if ( team >= 0 && !tiedForFirst ) {
-				if ( !fragLimitTimeout ) {
-					common->DPrintf( "enter sudden death FragLeader timeout, team %d is leader\n", team );
-					fragLimitTimeout = gameLocal.time + FRAGLIMIT_DELAY;
-				}
-				if ( gameLocal.time > fragLimitTimeout ) {
-					NewState( GAMEREVIEW );
-					gameLocal.mpGame.PrintMessageEvent( -1, MSG_FRAGLIMIT, team );
-				}
-			} else if ( fragLimitTimeout ) {
-				gameLocal.mpGame.PrintMessageEvent( -1, MSG_HOLYSHIT );
-				fragLimitTimeout = 0;
-			} 
-
 			break;
 		}
 	}
